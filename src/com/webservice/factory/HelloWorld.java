82e47fb529bae11a7a0f7a412874d22b76609e88
primayoriko <primayoriko@gmail.com>
2020-11-15 14:25:17 +0700

    make template

---
+++ src/com/webservice/factory/HelloWorld.java
@@ -0,0 +1,24 @@
+package com.webservice.factory;
+
+import javax.jws.WebMethod;
+import javax.jws.WebService;
+import javax.jws.soap.SOAPBinding;
+import javax.jws.soap.SOAPBinding.Style;
+import javax.xml.ws.Endpoint;
+
+//@WebService()
+@WebService
+@SOAPBinding(style = Style.RPC)
+public class HelloWorld {
+  @WebMethod
+  public String sayHelloWorldFrom(String from) {
+    String result = "Hello, world, from " + from;
+    System.out.println(result);
+    return result;
+  }
+  public static void main(String[] argv) {
+    Object implementor = new HelloWorld ();
+    String address = "http://localhost:9000/HelloWorld";
+    Endpoint.publish(address, implementor);
+  }
+}
