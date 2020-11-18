eefc8aecad2be366d94076079925dad9c7dba467
primayoriko <primayoriko@gmail.com>
2020-11-18 12:44:36 +0700

    add routes in sun-jaxws.xml

--- src/com/factory/webservices/Main.java
+++ src/com/factory/webservices/Main.java
@@ -5,19 +5,22 @@ import javax.jws.soap.SOAPBinding;
 import javax.jws.soap.SOAPBinding.Style;
 import javax.xml.ws.Endpoint;
 
+import javax.jws.WebMethod;
+
 @WebService
 @SOAPBinding(style = Style.RPC)
 public class Main {
-//  @WebMethod
-//  public String bonjour(String name) {
-//    return String.format("Bonjour %s", name);
-//  }
+  @WebMethod
+  public String bonjour(String name) {
+    return String.format("Bonjour %s", name);
+  }
   public static void main(String[] argv) {
     final String address = "http://localhost:9000/ws/";
-    Endpoint.publish(address + "Balance", new BalanceServiceImpl());
-    Endpoint.publish(address + "Chocolate", new ChocolateServiceImpl());
-    Endpoint.publish(address + "Ingredient", new IngredientServiceImpl());
-    Endpoint.publish(address + "Recipe", new RecipeServiceImpl());
-    Endpoint.publish(address + "Stock", new StockServiceImpl());
+//    Endpoint.publish(address + "Balance", new BalanceServiceImpl());
+//    Endpoint.publish(address + "Chocolate", new ChocolateServiceImpl());
+//    Endpoint.publish(address + "Ingredient", new IngredientServiceImpl());
+//    Endpoint.publish(address + "Recipe", new RecipeServiceImpl());
+//    Endpoint.publish(address + "Stock", new StockServiceImpl());
+    Endpoint.publish(address + "Main", new Main());
   }
 }
